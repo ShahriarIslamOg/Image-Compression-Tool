db701ee68a5be3d9bfe70e55c11865bab610a01b
Shahriar Islam <85397003+ByteShahriar@users.noreply.github.com>
2023-12-06 01:39:00 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,95 @@
-function compressImage() {
-    const input = document.getElementById('imageInput');
-    const originalImage = document.getElementById('originalImage');
-    const downloadLink = document.getElementById('downloadLink');
+document.addEventListener("DOMContentLoaded", () => {
+    const dropArea = document.getElementById('dropArea');
 
-    const file = input.files[0];
+    ['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+        dropArea.addEventListener(eventName, preventDefaults, false);
+        document.body.addEventListener(eventName, preventDefaults, false);
+    });
 
-    if (file) {
-        const reader = new FileReader();
+    ['dragenter', 'dragover'].forEach(eventName => {
+        dropArea.addEventListener(eventName, highlight, false);
+    });
 
-        reader.onload = function (e) {
-            const img = new Image();
-            img.src = e.target.result;
+    ['dragleave', 'drop'].forEach(eventName => {
+        dropArea.addEventListener(eventName, unhighlight, false);
+    });
 
-            img.onload = function () {
-                const canvas = document.createElement('canvas');
-                const ctx = canvas.getContext('2d');
+    dropArea.addEventListener('drop', handleDrop, false);
 
-                canvas.width = img.width;
-                canvas.height = img.height;
+    document.getElementById('fileInput').addEventListener('change', handleFiles, false);
+});
 
-                ctx.drawImage(img, 0, 0, img.width, img.height);
+function preventDefaults(e) {
+    e.preventDefault();
+    e.stopPropagation();
+}
 
-                canvas.toBlob((blob) => {
-                    const compressedUrl = URL.createObjectURL(blob);
-                    originalImage.src = compressedUrl;
-                    downloadLink.href = compressedUrl;
-                    downloadLink.style.display = 'inline';
-                }, 'image/jpeg', 0.8);
+function highlight() {
+    document.getElementById('dropArea').classList.add('highlight');
+}
+
+function unhighlight() {
+    document.getElementById('dropArea').classList.remove('highlight');
+}
+
+function handleDrop(e) {
+    const files = e.dataTransfer.files;
+    handleFiles(files);
+}
+
+function handleFiles(files) {
+    const resultDiv = document.getElementById('result');
+    resultDiv.innerHTML = '';
+
+    for (const file of files) {
+        if (file.type.startsWith('image/')) {
+            const reader = new FileReader();
+
+            reader.onload = function (e) {
+                const img = new Image();
+                img.src = e.target.result;
+
+                img.onload = function () {
+                    resultDiv.innerHTML += `<p>Original Size: ${file.size / 1024} KB</p>`;
+                    resultDiv.innerHTML += `<p>Original Dimensions: ${img.width} x ${img.height}</p>`;
+
+                    const quality = document.getElementById('quality').value;
+                    const compressedImage = compressImage(img, quality);
+
+                    resultDiv.innerHTML += `<p>Compressed Size: ${compressedImage.size / 1024} KB</p>`;
+                    resultDiv.innerHTML += `<p>Compressed Dimensions: ${compressedImage.width} x ${compressedImage.height}</p>`;
+                    resultDiv.innerHTML += `<img src="${compressedImage.src}" alt="Compressed Image">`;
+                };
             };
-        };
 
-        reader.readAsDataURL(file);
+            reader.readAsDataURL(file);
+        } else {
+            resultDiv.innerHTML += `<p>${file.name} is not a valid image file.</p>`;
+        }
     }
+}
+
+function compressImage(img, quality) {
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+    canvas.width = img.width;
+    canvas.height = img.height;
+    ctx.drawImage(img, 0, 0, img.width, img.height);
+
+    const dataUrl = canvas.toDataURL('image/jpeg', quality / 100);
+    const compressedImage = new Image();
+    compressedImage.src = dataUrl;
+
+    return {
+        src: dataUrl,
+        size: dataUrl.length,
+        width: compressedImage.width,
+        height: compressedImage.height
+    };
+}
+
+function compressImages() {
+    const fileInput = document.getElementById('fileInput');
+    const files = fileInput.files;
+    handleFiles(files);
 }
